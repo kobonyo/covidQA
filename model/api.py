2966c5b3c1458fcb60eec13b700b4f912779ba9a
KObonyo <stephenobonyo80@gmail.com>
2020-05-30 17:20:10 +0300

    model 5.30.2020

--- model/api.py
+++ model/api.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask import request
 from models import Models
 model = Models()
 app = Flask(__name__)
+
 @app.route('/api')
-def index():
+def index(methods=['GET']):
     q = request.args.get('q')
-    return model.predict(q)
+    res = jsonify({'answer': model.predict(q)})
+    return res
 
 if __name__ == '__main__':
-    app.run(debug = True )
+    app.run(host = '0.0.0.0', debug = True )
